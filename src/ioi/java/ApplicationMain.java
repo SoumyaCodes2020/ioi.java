9eaf27fe40afba389962dc235988c1dc348f56eb
Munmun <Munmun@SahaFamily.litdom.lit.ie>
2020-01-18 15:38:33 +0000

    version 1

---
+++ src/ioi/java/ApplicationMain.java
@@ -0,0 +1,40 @@
+/*
+ To change this license header, choose License Headers in Project Properties.
+ To change this template file, choose Tools | Templates
+ and open the template in the editor.
+ */
+package ioi.java;
+
+
+/**
+
+ @author Soumyadeep
+  */
+import java.util.Scanner;
+public class ApplicationMain
+{
+
+	/**
+	 @param args the command line arguments
+	 */
+	public static void main(String[] args)
+        { 
+            
+   String name ="Soumyadeep saha";         
+  int age = 10;
+   int number1 =10;     
+     int number2 =5;
+     
+     int answer = number1 + number2;
+      
+            System.out.println("name");
+            System.out.println("age");
+            System.out.println("answer");
+            
+            System.out.println("My name is " + name + " ,my age is " + age +"my answer is " +answer);
+            System.out.println("outputText");
+         }
+} 
+
+
+
